f898075d5fc8b1c6a20c8619fad500a808e821c1
AliHaider6325 <meharbanali6325@gmail.com>
2025-08-31 22:00:18 +0500

    Final Commit, Fully Optimized 90+ Performance

--- src/components/MyDetails.tsx
+++ src/components/MyDetails.tsx
@@ -10,7 +10,7 @@ const MyDetails = () => {
         I’m <b>Muhammad Ali Haider</b>, a developer dedicated to making the
         world a better place one line of code at a time.
       </p>
-      <a href="/Ali'sCv.pdf" download={"Ali's Cv"}>
+      <a href="/Ali-Cv.pdf" download="Ali-Cv">
         <button className="flex items-center justify-center p-4 bg-[#E5F4E3] text-black font-bold rounded-2xl mt-3 cursor-pointer w-full overflow-hidden relative group">
           <FcVoicePresentation className="mr-2 text-2xl" />
           <span className="relative z-10">Get Resume</span>
